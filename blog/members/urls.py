acdd0a3d77bdb0e29bedfcd24ea8bfea647b2d18
Michal Zawadzki <m.z.zawadzkimichal@gmail.com>
2024-02-13 14:32:25 +0000

    html edit for blog post

--- blog/members/urls.py
+++ blog/members/urls.py
@@ -4,5 +4,6 @@ from .views import UserRegisterView
 
 
 urlpatterns = [
-    path('register/', UserRegisterView.as_view(), name='register')
+    path('register/', UserRegisterView.as_view(), name='register'),
+    path('/', include('django.contrib.auth.urls')),  # Remove the namespace parameter
 ]
--- blog/members/views.py
+++ blog/members/views.py
@@ -8,3 +8,4 @@ class UserRegisterView(generic.CreateView):
     template_name = 'registration/register.html'
     success_url = reverse_lazy('login')
 
+
